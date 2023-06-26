f1f5742aa2f1c84d60c46bf71957d34f777f6266
Vanessa Furquim <vanessafurquim.contato@gmail.com>
2023-06-26 17:34:50 -0300

    final commit: routes tweet and sign-up working; success messages ok;

--- src/app.js
+++ src/app.js
@@ -1,54 +1,59 @@
-import express, { json } from "express"
-import cors from "cors"
+import express, { json } from "express";
+import cors from "cors";
 
-const app = express()
-app.use(cors())
-app.use(json())
+const app = express();
+app.use(cors());
+app.use(json());
 
-const users = []
-const tweets = []
+const users = [];
+const tweets = [];
 
 app.post("/sign-up", (req, res) => {
-    users.push({ username: req.body.username, avatar: req.body.avatar})
-    res.send("Ok!")
-})
+  users.push({ username: req.body.username, avatar: req.body.avatar });
+  res.send("Ok!");
+});
 
 app.post("/tweets", (req, res) => {
 
-    /* if (usuário não estiver cadastrado = se username não estiver em [users]) = {
+        const registeredUser = users.find(
+            user => user.username === req.body.username
+        )
+        
+        if (registeredUser === undefined) {
         res.send("UNAUTHORIZED")
         return
-    }*/
+    }
 
-    /* tweets.push({username: req.body.username, tweet: req.body.tweet}) */
-    res.send("Ok!")
-})
+  tweets.push({ username: req.body.username, tweet: req.body.tweet });
+  res.send("Ok!");
+});
 
 app.get("/tweets", (req, res) => {
 
-    const singleUser = users.find((user) => {user.username === username.tweets})
+    const newTweets = [];
 
-    console.log()
+    const tenLastTweets = tweets.slice(-10)
 
-    const tweet = {
-        username: users.username,
-        avatar: singleUser.avatar,
-        tweet: tweets.tweet
-    }
-
-    tweets.push(tweet)
-
-    /* console.log(tweet) */
-
-    /* const tenLastTweets = tweets.slice(tweets.legth - 10, tweets.length)
-
-    if (req.tenLastTweets === undefined) {
+    if (tenLastTweets.length === 0) {
         res.send([])
         return
     }
 
-    res.send(tenLastTweets) */
-})
+    for (let i = 0; i < tenLastTweets.length; i++) {
 
-const PORT = 5000
-app.listen(PORT, () => console.log(`Server active on port ${PORT}.`) )
+        const singleUser = users.find((user) => user.username === tenLastTweets[i].username)
+        
+        const tweet = {
+            username: tenLastTweets[i].username,
+            avatar: singleUser.avatar,
+            tweet: tenLastTweets[i].tweet
+        }
+
+        newTweets.push(tweet)
+
+    }
+  res.send(newTweets)
+});
+
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Server active on port ${PORT}.`));
