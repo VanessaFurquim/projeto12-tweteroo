2f3f97008bc508338ce841978253401a70f29b59
Vanessa Furquim <vanessafurquim.contato@gmail.com>
2023-08-13 17:51:48 -0300

    post/sign-up: tested and running; post/tweets: tested and running; validations tested and running; status codes applied

--- src/app.js
+++ src/app.js
@@ -1,59 +1,66 @@
-import express, { json } from "express";
-import cors from "cors";
+import express, { json } from "express"
+import cors from "cors"
 
-const app = express();
-app.use(cors());
-app.use(json());
+const app = express()
+app.use(json())
+app.use(cors())
 
-const users = [];
-const tweets = [];
+const users = []
+const tweets = []
 
 app.post("/sign-up", (req, res) => {
-  users.push({ username: req.body.username, avatar: req.body.avatar });
-  res.send("Ok!");
-});
+
+  const { username, avatar } = req.body
+
+  if (username === "" || typeof(username) !== "string" || typeof(username) === undefined) {
+    return res.status(400).send("Todos os campos são obrigatórios!")
+  }
+
+  if (avatar === "" || typeof(avatar) !== "string" || typeof(avatar) === undefined) {
+    return res.status(400).send("Todos os campos são obrigatórios!")
+  }
+
+  users.push({ username: username, avatar: avatar })
+  res.status(201).send("Ok!")
+})
 
 app.post("/tweets", (req, res) => {
 
-        const registeredUser = users.find(
-            user => user.username === req.body.username
-        )
-        
-        if (registeredUser === undefined) {
-        res.send("UNAUTHORIZED")
-        return
-    }
+  const { username, tweet } = req.body
 
-  tweets.push({ username: req.body.username, tweet: req.body.tweet });
-  res.send("Ok!");
-});
+  if(!username || !tweet) return res.send("Preencha todos os campos!")
+
+  const registeredUser = users.find(user => user.username === username)
+        
+  if (!registeredUser) return res.status(401).send("UNAUTHORIZED")
+        
+  tweets.push({ username: username, tweet: tweet })
+  res.status(201).send("Ok!")
+})
 
 app.get("/tweets", (req, res) => {
 
-    const newTweets = [];
+  const newTweets = []
 
-    const tenLastTweets = tweets.slice(-10)
+  const tenLastTweets = tweets.slice(-10)
 
-    if (tenLastTweets.length === 0) {
-        res.send([])
-        return
-    }
+  if (tenLastTweets.length === 0) return res.send([])
 
-    for (let i = 0; i < tenLastTweets.length; i++) {
+  for (let i = 0; i < tenLastTweets.length; i++) {
 
-        const singleUser = users.find((user) => user.username === tenLastTweets[i].username)
+    const singleUser = users.find(user => user.username === tenLastTweets[i].username)
         
-        const tweet = {
-            username: tenLastTweets[i].username,
-            avatar: singleUser.avatar,
-            tweet: tenLastTweets[i].tweet
-        }
-
-        newTweets.push(tweet)
-
+    const tweet = {
+      username: tenLastTweets[i].username,
+      avatar: singleUser.avatar,
+      tweet: tenLastTweets[i].tweet
     }
+
+    newTweets.push(tweet)
+
+  }
   res.send(newTweets)
-});
+})
 
 const PORT = 5000;
-app.listen(PORT, () => console.log(`Server active on port ${PORT}.`));
+app.listen(PORT, () => console.log(`Server active on port ${PORT}.`))
